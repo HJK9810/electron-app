51cf55a0e2f6ae1de00ed190655b16a8ed9736ce
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-09-15 20:26:26 +0900

    add component use btn

--- src/MainScreen.jsx
+++ src/MainScreen.jsx
@@ -1,21 +1,29 @@
 import { Container } from "react-bootstrap";
+import { useState } from "react";
 // import Board from "./vexflow/board.vexflow";
 import Draw from "./vexflow/draw.vexflow";
 
 function Main() {
+  const [draw, setDraw] = useState([]);
+  const addDraw = () => {
+    const ary = [
+      ["g2", "d4", "e4", "d4"],
+      ["a4", "d4", "e4", "d4"],
+      ["a4", "a4", "b4", "a4"],
+      ["d4", "e4", ["g3", 2]],
+    ];
+    setDraw(draw.concat(<Draw staves={ary} />));
+  };
+
   return (
     <Container>
       <button id="reload" onClick={(e) => location.reload()}>
         새로고침
       </button>
-      <Draw
-        staves={[
-          ["g2", "d4", "e4", "d4"],
-          ["a4", "d4", "e4", "d4"],
-          ["a4", "a4", "b4", "a4"],
-          ["d4", "e4", ["g3", 2]],
-        ]}
-      ></Draw>
+      <button id="reload" onClick={addDraw}>
+        악보추가
+      </button>
+      {draw}
     </Container>
   );
 }
