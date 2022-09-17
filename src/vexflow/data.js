6599a0128b206e075fccc6afc6bab617860f140f
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-09-17 15:30:25 +0900

    for data sotrage

---
+++ src/vexflow/data.js
@@ -0,0 +1,13 @@
+// For storage notes
+/**
+ * format
+ * {
+ *    index : [[notes], [notes], [notes], [notes]] - 각 마디별 한 ary, 들어가는 음표들
+ * }
+ * index -> 악보 순서
+ * [notes] -> "계이름 + 높낮이" or ["계이름 + 높낮이", 박자]
+ *  박자가 존재 X ? 4분음표 : 그 외(16, 8, 2, 온음표)
+ */
+let data = {};
+
+export default data;
