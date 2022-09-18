c713eb29bf8940c22ecff2e72ae1c487631f99ab
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-09-18 15:29:46 +0900

    print input data

--- src/MainScreen.jsx
+++ src/MainScreen.jsx
@@ -2,20 +2,17 @@ import { Container } from "react-bootstrap";
 import { useState } from "react";
 import Draw from "./vexflow/draw.vexflow";
 import InputBtn from "./vexflow/input.vexflow";
+import BarCard from "./vexflow/OneBar.vexflow";
+import data from "./vexflow/data";
 
 function Main() {
   const [draw, setDraw] = useState([]);
   const [show, setShow] = useState("none");
-  const [drawKey, setDrawKey] = useState(0);
+  const [drawKey, setDrawKey] = useState(1);
 
   const addDraw = () => {
     setDrawKey(drawKey + 1);
-    const ary = [
-      ["g2", "d4", "e4", "d4"],
-      ["a4", "d4", "e4", "d4"],
-      ["a4", "a4", "b4", "a4"],
-      ["d4", "e4", ["g3", 2]],
-    ];
+    const ary = data[drawKey];
     setDraw(draw.concat(<Draw key={drawKey} staves={ary} />));
   };
 
@@ -24,10 +21,7 @@ function Main() {
       <button id="reload" onClick={(e) => location.reload()}>
         새로고침
       </button>
-      <button
-        id="reload"
-        onClick={(e) => (show == "none" ? setShow("block") : setShow("none"))}
-      >
+      <button id="reload" onClick={(e) => (show == "none" ? setShow("block") : setShow("none"))}>
         음표추가
       </button>
       <button id="reload" onClick={addDraw}>
@@ -35,6 +29,9 @@ function Main() {
       </button>
       <div style={{ display: show }}>
         <InputBtn />
+      </div>
+      <div style={{ display: show }}>
+        <BarCard index={drawKey} />
       </div>
       {draw}
     </Container>
