a5427c4a83b9a05c5b873a93d935495835761950
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-09-18 09:28:13 +0900

    modify for data storage

--- src/vexflow/input.vexflow.jsx
+++ src/vexflow/input.vexflow.jsx
@@ -8,34 +8,29 @@ function InputBtn() {
   const beats = [16, 8, 4, 2, 1];
   const obj = data;
   let idx = Object.keys(obj).length ? Object.keys(obj).length : 1;
-  const ary = idx in obj ? obj[idx] : [];
+  const ary = idx in obj ? obj[idx] : [[]];
 
   if (ary.length == 4) {
     let sum = 0;
     ary.map((el) => {
       Array.isArray(el) ? (sum += 1 / el[1]) : (sum += 1 / 4);
     });
-    if (sum === 1) idx++;
+    if (sum == 1) idx++;
   }
 
   const forSubmit = () => {
     const input = parseInt(beat) === 4 ? sylChage + "4" : [sylChage + "4", parseInt(beat)];
-    const inCheck = false;
     ary.map((el) => {
       let sum = 0;
       el.map((element) => {
         Array.isArray(element) ? (sum += 1 / element[1]) : (sum += 1 / 4);
       });
-      if (sum === 1) inCheck = true;
-      else {
-        el.push(input);
-        inCheck = true;
-      }
+      if (sum === 1) ary.push([input]);
+      else if (sum + 1 / parseInt(beat) > 1) return alert("사용 불가능한 박자입니다. 다른것을 선택해주세요."); // roop out and need beat change
+      else el.push(input);
     });
 
-    if (!inCheck) ary.push([input]);
     data[idx] = ary;
-    console.log(data);
   };
 
   return (
