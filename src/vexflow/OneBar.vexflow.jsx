6f7aa8ed09703707b583dfc99bb0eff41a35d69a
HJ_Kim <50227588+HJK9810@users.noreply.github.com>
2022-09-20 21:12:57 +0900

    change indexof to includes

--- src/vexflow/OneBar.vexflow.jsx
+++ src/vexflow/OneBar.vexflow.jsx
@@ -5,7 +5,7 @@ const BarCard = ({notes = [[]]}) => {
 
   useEffect(() => {
     setAry(notes);
-  }, [notes]);
+  }, []);
 
   return (
     <div>
@@ -14,10 +14,11 @@ const BarCard = ({notes = [[]]}) => {
           {el.map((element, idx) => {
             let line = "";
             if (Array.isArray(element)) {
-              if (element[1].indexOf("r") != -1) {
-                line = parseInt(element[1].replace("r", "")) == 1 ? element[0] + " 온쉼표" : element[0] + " " + element[1] + "분쉼표";
+              const beat = element[1];
+              if (beat && beat.includes("r")) {
+                line = parseInt(beat.replace("r", "")) == 1 ? element[0] + " 온쉼표" : element[0] + " " + beat + "분쉼표";
               } else {
-                line = element[1] == 1 ? element[0] + " 온음표" : element[0] + " " + element[1] + "분음표";
+                line = beat == 1 ? element[0] + " 온음표" : element[0] + " " + beat + "분음표";
               }
             } else line = element + " 4분음표";
             return <div key={i + idx / 100}>{line}</div>;
