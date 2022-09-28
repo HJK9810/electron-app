900574cb8408165f52ac1b691093a36bc2fa66e7
HJ_Kim <ruby45342@gmail.com>
2022-09-28 10:56:42 +0900

    modify for fill rest note

--- src/vexflow/FillRest.js
+++ src/vexflow/FillRest.js
@@ -1,9 +1,11 @@
-const beats = [16, 8, 4, 2];
+import {StaveNote} from "vexflow";
+
+const beats = [16, 8, 4, 2, 1];
 
 function fillNote(left) {
   let rest = [];
 
-  for (let i = 3; i >= 0; i--) {
+  for (let i = 4; i >= 0; i--) {
     const num = Math.floor(left * beats[i]);
     rest.unshift(num);
     left -= num / beats[i];
@@ -16,15 +18,22 @@ const fillRestNote = (notes) => {
   notes.forEach((note) => {
     let sum = 0;
     for (let i = 0; i < note.length; i++) {
-      if (Array.isArray(note[i])) {
-        sum += 1 / parseInt((note[i][1] + "").replace("r", ""));
-      } else sum += 1 / 4;
+      if (note[i].customTypes[0] !== "r") {
+        const beat = note[i].duration == "w" ? 1 : parseInt(note[i].duration);
+        sum += 1 / beat;
+      }
     }
     if (sum != 1) {
       const rest = fillNote(1 - sum);
       rest.map((el, idx) => {
         for (let i = 0; i < el; i++) {
-          note.push(["b4", beats[idx] + "r"]);
+          if (idx == 4) continue;
+          note.push(
+            new StaveNote({
+              keys: ["b/4"],
+              duration: beats[idx],
+            })
+          );
         }
       });
     }
